85e4b5f1d51c3b830f8974a40be982306fcefc24
ishant <ishant360@gmail.com>
2018-02-15 23:48:23 +0000

    redesign after peer feedback

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -2,5 +2,6 @@ import '../css/main.scss';
 import '../css/heading.scss';
 import '../css/skills.scss';
 import '../css/experience.scss';
+import '../css/quote.scss';
 
 console.log('hello world from rollup and live reload');
