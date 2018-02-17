11df1c831d6f7246739ddb24dc5c7ce199a7cb0a
ishant <ishant360@gmail.com>
2018-02-17 00:31:31 +0000

    Fixed cache busting

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,21 +1,24 @@
 import sass from 'rollup-plugin-sass';
+import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
 
-const randomGenerator = () => Math.ceil(Math.random().toFixed(4) * 10000);
-
-const randomNumber = randomGenerator();
 export default {
     input: 'dist/javascript/main.js',
     output: {
-        file: `docs/bundle.${randomNumber}.js`,
+        file: `docs/bundle.js`,
         format: 'cjs'
     },
     plugins: [
         sass({
             insert: false,
-            output: `docs/bundle.${randomNumber}.css`,
+            output: `docs/bundle.css`,
             options: {
                 sourceMap: false
             }
+        }),
+        replaceHtmlVars({
+            files: 'docs/index.html',
+            from: /\${timestamp}/g,
+            to: Date.now(),
         })
     ]
   };
